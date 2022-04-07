d4c396ea9a00a9af07b4b08710f2d93bff3aee68
Nico Papamichael <nico.papamichael@gmail.com>
2022-04-07 10:52:52 +0100

    added support for .ag and relevant test files

--- whois/parser.py
+++ whois/parser.py
@@ -410,6 +410,8 @@ class WhoisEntry(dict):
             return WhoisBio(domain, text)
         elif domain.endswith('.ac'):
             return WhoisAc(domain, text)
+        elif domain.endswith('.ag'):
+            return WhoisAg(domain, text)
         else:
             return WhoisEntry(domain, text)
 
@@ -4509,4 +4511,32 @@ class WhoisAc(WhoisEntry):
         else:
             WhoisEntry.__init__(self, domain, text, self.regex)
 
-
+class WhoisAg(WhoisEntry):
+    regex = {
+        'domain_name':                    r'Domain Name: *(.+)',
+        'registry_domain_id':             r'Registry Domain ID: *(.+)',
+        'registrar_whois_server':         r'Registrar WHOIS Server: *(.+)',
+        'registrar_url':                  r'Registrar URL: *(.+)',
+        'updated_date':                   r'Updated Date: *(.+)',
+        'creation_date':                  r'Creation Date: *(.+)',
+        'registry_expiry_date':           r'Registry Expiry Date: *(.+)',
+        'registrar_registration_exp_date':r'Registrar Registration Expiration Date: *(.+)',
+        'registrar':                      r'Registrar: *(.+)',  
+        'registrar_iana_id':              r'Registrar IANA ID: *(.+)',
+        'reseller':                       r'Reseller: *(.+)', 
+        'status':                         r'Domain Status: *(.+)',   
+        'admin_email':                    r'Admin Email: *(.+)',
+        'registrant_org':                 r'Registrant Organization: *(.+)',
+        'registrant_state/province':      r'Registrant State/Province: *(.+)',
+        'registrant_country':             r'Registrant Country: *(.+)',
+        'name_server':                    r'Name Server: *(.+)',
+        'dnssec':                         r'DNSSEC: *(.+)',
+        'registrar_abuse_contact_email':  r'Registrar Abuse Contact Email: *(.+)',
+        'registrar_abuse_contact_phone':  r'Registrar Abuse Contact Phone: *(.+)',
+        'url_of_icann_form':              r'URL of the ICANN Whois Inaccuracy Complaint Form: *(.+)',
+    }
+    def __init__(self, domain, text):
+        if 'Not found:' in text:
+            raise PywhoisError(text)
+        else:
+            WhoisEntry.__init__(self, domain, text, self.regex)
