81c9fbcd6e47973c2a672ff0b11406ba971baca1
Sercan Bayrambey <sercanbayrambeyy@gmail.com>
2023-11-06 13:09:21 +0300

    fix: site domain tld conflict errors

--- whois/parser.py
+++ whois/parser.py
@@ -3372,6 +3372,8 @@ class WhoisSite(WhoisEntry):
     def __init__(self, domain, text):
         if "DOMAIN NOT FOUND" in text:
             raise PywhoisError(text)
+        else:
+            WhoisEntry.__init__(self, domain, text, self.regex)
 
           
 class WhoisEdu(WhoisEntry):
