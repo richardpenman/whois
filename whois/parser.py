cf3ac41fe97e321711780afd7276afb05a7794b6
wakaka6 <48764488+wakaka6@users.noreply.github.com>
2023-11-20 16:27:57 +0800

    Fix indent error

--- whois/parser.py
+++ whois/parser.py
@@ -3371,8 +3371,11 @@ class WhoisSite(WhoisEntry):
 
     def __init__(self, domain, text):
         if "DOMAIN NOT FOUND" in text:
+            raise PywhoisError(text)
+        else:
+            WhoisEntry.__init__(self, domain, text, self.regex)
 
-          
+
 class WhoisEdu(WhoisEntry):
     """Whois parser for .edu domains"""
     regex = {
