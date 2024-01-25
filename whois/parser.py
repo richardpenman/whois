2b37d11fe8fbc4f7d7b0dd78567df6e6afd70563
Adam Ponce <94799223+adamcysec@users.noreply.github.com>
2024-01-25 15:15:14 -0600

    Update parser.py
    fix IndentationError caused by an incomplete if statement on line 3373.

--- whois/parser.py
+++ whois/parser.py
@@ -3371,7 +3371,9 @@ class WhoisSite(WhoisEntry):
 
     def __init__(self, domain, text):
         if "DOMAIN NOT FOUND" in text:
-
+            raise PywhoisError(text)
+        else:
+            WhoisEntry.__init__(self, domain, text, self.regex)
           
 class WhoisEdu(WhoisEntry):
     """Whois parser for .edu domains"""
