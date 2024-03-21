dae6ecd05dc1caf5b3e76c27dd8f312737e9e7bc
Richard Penman <{ID}+{username}@users.noreply.github.com>
2024-03-21 21:05:34 +0800

    bumped version for pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(filename):
 
 setuptools.setup(
     name='python-whois',
-    version='0.8.2',
+    version='0.9.0',
     description="Whois querying and parsing of domain registration information.",
     long_description=read('README.rst'),
     install_requires=[
