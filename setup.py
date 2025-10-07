61af95f5c2310dfb7a4fa4fae26543e50464a168
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2025-10-07 12:38:40 +0800

    bumped version for pypi

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(filename):
 
 setuptools.setup(
     name="python-whois",
-    version="0.9.5",
+    version="0.9.6",
     description="Whois querying and parsing of domain registration information.",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
